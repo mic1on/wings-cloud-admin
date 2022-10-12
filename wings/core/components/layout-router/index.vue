bf484dbc4ad65248e8aa581c08e0b4721936e461
wangfunfun <kaivanwong@outlook.com>
2022-10-12 23:03:13 +0800

    version 0.6.11-beta

---
+++ wings/core/components/layout-router/index.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+import { useRoute } from 'vue-router';
+const route = useRoute();
+const isKeepAlive = import.meta.env.APP_KEEP_ALIVE;
+</script>
+
+<template>
+  <router-view v-slot="{ Component }">
+    <keep-alive v-if="isKeepAlive">
+      <component :is="Component" :key="route.fullPath" />
+    </keep-alive>
+    <component :is="Component" v-else />
+  </router-view>
+</template>
