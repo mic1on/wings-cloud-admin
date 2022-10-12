df8f1c048c5a0492b5dd3fb7cdc6dc30871287ea
wangfunfun <kaivanwong@outlook.com>
2022-10-12 22:35:47 +0800

    version 0.6.10-beta

---
+++ wings/core/components/action-avatar/avatar.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+import { RouteEnum, useWingsStore } from '@wings';
+
+const { t } = useI18n();
+const router = useRouter();
+
+const { appUserStore } = useWingsStore();
+
+const goLoginPage = (): void => {
+  if (appUserStore.isLogin) return;
+  router.push({ path: RouteEnum.ROUTE_LOGIN });
+};
+</script>
+
+<template>
+  <el-avatar
+    cursor-pointer
+    :size="34"
+    :src="appUserStore.userInfo.avatar"
+    @click="goLoginPage()"
+  >
+    <span text-1>{{ appUserStore.isLogin ? '' : t('base.form.login') }}</span>
+  </el-avatar>
+</template>
--- wings/core/components/action-avatar/index.vue
+++ wings/core/components/action-avatar/index.vue
@@ -1,24 +1,60 @@
 <script lang="ts" setup>
-import { RouteEnum, useWingsStore } from '@wings';
+import useUserStore from '../../plugins/pinia/modules/user';
+import { RouteEnum } from '../../enums';
+import Avatar from './avatar.vue';
 
 const { t } = useI18n();
+
+const route = useRoute();
+
 const router = useRouter();
 
-const { appUserStore } = useWingsStore();
+const userStore = useUserStore();
 
-const goLoginPage = (): void => {
-  if (appUserStore.isLogin) return;
-  router.push({ path: RouteEnum.ROUTE_LOGIN });
+const adminRoute = import.meta.env.APP_ADMIN_FIRST_ROUTE;
+
+const actionChange = (command: string): void => {
+  if (command.indexOf('/') !== -1) {
+    router.push({ path: command });
+  } else if (command === 'switchRoles') {
+    userStore.switchRoles();
+  } else if (command === 'logout') {
+    userStore.logout(
+      route.path.indexOf('/website/') !== -1 ? 'refresh' : 'login'
+    );
+  }
 };
 </script>
-
 <template>
-  <el-avatar
-    cursor-pointer
-    :size="34"
-    :src="appUserStore.userInfo.avatar"
-    @click="goLoginPage()"
-  >
-    <span text-1>{{ appUserStore.isLogin ? '' : t('base.form.login') }}</span>
-  </el-avatar>
+  <avatar v-if="!userStore.isLogin"> </avatar>
+  <el-dropdown @command="actionChange" v-else>
+    <avatar> </avatar>
+    <template #dropdown>
+      <el-dropdown-menu>
+        <el-dropdown-item
+          :command="adminRoute"
+          v-if="route.path.indexOf('website') !== -1"
+        >
+          <el-icon><Monitor /></el-icon>
+          <span>{{ t('base.admin') }}</span>
+        </el-dropdown-item>
+        <el-dropdown-item command="swtichRoles">
+          <el-icon><CollectionTag /></el-icon>
+          <span>{{ t('base.switchRoles') }}</span>
+        </el-dropdown-item>
+        <el-dropdown-item :command="RouteEnum.ROUTE_PERSONAL_PROFILE">
+          <el-icon><User /></el-icon>
+          <span>{{ t('base.userInfo') }}</span>
+        </el-dropdown-item>
+        <el-dropdown-item :command="RouteEnum.ROUTE_PERSONAL_SETTINGS">
+          <el-icon><Setting /></el-icon>
+          <span>{{ t('base.userSettings') }}</span>
+        </el-dropdown-item>
+        <el-dropdown-item command="logout">
+          <el-icon><SwitchButton /></el-icon>
+          <span>{{ t('base.form.logout') }}</span>
+        </el-dropdown-item>
+      </el-dropdown-menu>
+    </template>
+  </el-dropdown>
 </template>
