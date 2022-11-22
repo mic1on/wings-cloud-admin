eaa5a5353acba22ca86b2f422fd457cfc6d93027
wangfunfun <kaivanwong@outlook.com>
2022-11-22 17:37:08 +0800

    bug fixes: 修复 declare.d.ts

---
+++ src/declare.d.ts
@@ -0,0 +1,12 @@
+declare module '*.svg';
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.gif';
+declare module '*.bmp';
+declare module '*.tiff';
+declare module '*.gif';
+declare module 'qs';
+declare module 'mockjs';
+declare module 'element-plus/dist/locale/en.mjs';
+declare module 'element-plus/dist/locale/zh-cn.mjs';
