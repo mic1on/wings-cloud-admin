29c75b33f7554ac254b3bedbd50bac8b5529f64d
wangfunfun <kaivanwong@outlook.com>
2022-11-25 15:34:47 +0800

    feat: 更新 preivew 命令；

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import type { App } from 'vue';
 import { createApp } from 'vue';
+import 'virtual:svg-icons-register';
 import _App from './components/app/app.vue';
 
 import { useElementPlus, useElementPlusIcons } from './plugins/element-plus';
@@ -8,11 +9,9 @@ import { useRouter } from './plugins/vue-router';
 import { useI18n } from './plugins/vue-i18n';
 import { usePinia } from './plugins/pinia';
 import { useMock } from './plugins/mock';
-import 'virtual:svg-icons-register';
 
 import 'element-plus/theme-chalk/src/message.scss';
 import 'element-plus/theme-chalk/src/notification.scss';
-
 import './assets/themes/index.scss';
 import './assets/styles/index.scss';
 import 'uno.css';
